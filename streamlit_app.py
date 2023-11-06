11907fb5fd7fa1d96c659c2dc35e8ec72d817ea4
Subir Mansukhani <subir.mansukhani@dominodatalab.com>
2023-11-05 19:15:59 -0800

    Pushed from Domino: https://demo2.dominodatalab.com/workspace/subir_mansukhani/RAG-mktg?executionId=654573c6e739e077722940da

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,6 @@ embeddings = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-small-en",
                                       encode_kwargs=encode_kwargs
                                      )
 
-doc_store = Qdrant.from_texts(texts,
-                          metadatas=metadatas,
-                          embedding=embeddings,
-                          url=qdrant_url,
-                          api_key=os.environ['QDRANT_API_KEY'],
-                          collection_name=f"medical_qa_search")
-
 
 prompt_template = """Use the following pieces of context to answer the question enclosed within  3 backticks at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Please provide an answer which is factually correct and based on the information retrieved from the vector store.
@@ -68,7 +61,7 @@ qdrant_key = st.sidebar.text_input("Enter your Anthropic API key", type="passwor
 clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
 qa_chain = None
-
+doc_store = None
 
 if clear_button:
     st.session_state['generated'] = []
@@ -77,6 +70,16 @@ if clear_button:
         {"role": "system", "content": "You are a helpful assistant."}
     ]
     memory.clear()
+    
+
+if qdrant_key:
+    doc_store = Qdrant.from_texts(texts,
+                          metadatas=metadatas,
+                          embedding=embeddings,
+                          url=qdrant_url,
+                          api_key=qdrant_key,
+                          collection_name=f"medical_qa_search")
+
 
 
 if doc_store and anthropic_key:
@@ -99,7 +102,7 @@ with container:
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area("You:", key='input', height=100)
         submit_button = st.form_submit_button(label='Send')
-    if submit_button and user_input and qa_chain and anthropic_key:
+    if submit_button and user_input and qa_chain:
         with st.spinner("Searching for the answer..."):
             result = qa_chain(user_question)
         answer = result["result"]
