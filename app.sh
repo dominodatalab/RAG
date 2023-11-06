e95433982503054e167cd1fb45aa14ec61d0544d
Subir Mansukhani <subir.mansukhani@dominodatalab.com>
2023-11-05 17:46:49 -0800

    Pushed from Domino: https://demo2.dominodatalab.com/workspace/subir_mansukhani/RAG-mktg?executionId=654573c6e739e077722940da

--- app.sh
+++ app.sh
@@ -2,7 +2,7 @@ mkdir ~/.chainlit
 echo "[project]" > ~/.chainlit/config.toml
 echo "enable_telemetry = true" >> ~/.chainlit/config.toml
 echo "session_timeout = 3600" >> ~/.chainlit/config.toml
-echo "[ UI]" >> ~/.chainlit/config.toml
+echo "[UI]" >> ~/.chainlit/config.toml
 echo "hide_cot = true" >> ~/.chainlit/config.toml
 cat << EOF >> ~/.chainlit/config.toml
 EOF
